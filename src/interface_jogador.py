4220d45abdbd644dc7852129d1da134886d7096c
Henrique1803 <henriquemateusteodoro@gmail.com>
2024-10-23 02:20:02 -0300

    refactor: coerência entre código e diagramas

--- src/interface_jogador.py
+++ src/interface_jogador.py
@@ -25,21 +25,22 @@ class InterfaceJogador(DogPlayerInterface):
         self.tela_inicial.configurar_tela()
         self.tela_inicial.tela_conexao_dog.abrir_tela()
         self.janela_principal.focus_force()
-
-    def iniciar_jogo(self):
-        self.tela_jogo.configurar_tela()
+    
+    def desbloquear_botao_iniciar(self):
+        self.tela_inicial.canvas.itemconfig(self.tela_inicial.botao_jogar, state="normal")
     
     def analisar_mensagem_dog(self, mensagem):
         if mensagem == "Conectado a Dog Server":
             self.tela_inicial.tela_conectado.abrir_tela()
-            self.tela_inicial.canvas.itemconfig(self.tela_inicial.botao_jogar, state="normal")
+            self.desbloquear_botao_iniciar()
         elif mensagem == "Não conectado a Dog Server":
             self.tela_inicial.tela_conexao_falhou.abrir_tela()
         elif mensagem == "Jogadores insuficientes":
             self.tela_inicial.tela_jogadores_insuficientes.abrir_tela()
         elif mensagem == "Partida iniciada":
             self.tela_inicial.tela_recebimento_partida.abrir_tela()
-            self.iniciar_jogo()
+            # instanciar Jogo aqui
+            self.tela_jogo.configurar_tela()
 
     def iniciar_partida(self):
         start_status = self.dog_server_interface.start_match(2)
